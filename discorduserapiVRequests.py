12e998291e2394c6469b0786fa23048361b4a322
Filigram <130276494+Filigram@users.noreply.github.com>
2024-01-16 15:48:16 +0400

    Add or update file via git

--- discorduserapiVRequests.py
+++ discorduserapiVRequests.py
@@ -6,8 +6,9 @@ from io import BytesIO
 
 class DiscordUserAPI:
 
-    def __init__(self, ouath) -> None:
+    def __init__(self, id, ouath) -> None:
         self.ouath = ouath
+        self.id = id
         self.headers_send_mdg = {
             "accept": "*/*",
             "accept-language": "fr,fr-FR;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
@@ -100,7 +101,7 @@ class DiscordUserAPI:
             pygame.time.Clock().tick(10)
 
     def get_friends_online(self, legacy_name: bool = False, raw: bool = False):
-        url = "https://discord.com/api/v9/users//profile?with_mutual_friends_count=true"
+        url = f"https://discord.com/api/v9/users/{self.id}/profile?with_mutual_friends_count=true"
         
         response = requests.get(
             url,
@@ -116,4 +117,4 @@ class DiscordUserAPI:
 
     
     def get_messages(self, channel_id: str, limit: str = "50", reverse_sort: bool = False):
-        url = "https://discord.com/api/v9/channels/{}/messages?limit=50"
+        url = f"https://discord.com/api/v9/channels/{channel_id}/messages?limit=50"
