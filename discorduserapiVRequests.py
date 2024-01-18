7d30e71b4d23b842c4a0e62151611cae46224095
Filigram <130276494+Filigram@users.noreply.github.com>
2024-01-18 17:00:39 +0400

    Add or update file via git

--- discorduserapiVRequests.py
+++ discorduserapiVRequests.py
@@ -279,6 +279,40 @@ class DiscordUserAPI:
         response = requests.post(url, headers=headers, json=data)
 
         return response.status_code
+    
+    def get_friends(self) -> list[dict]:
+        url = "https://discord.com/api/v9/users/@me/relationships"
+        headers = {
+            "accept": "*/*",
+            "accept-language": "fr,fr-FR;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
+            "authorization": self.ouath,
+            "cache-control": "no-cache",
+            "pragma": "no-cache",
+            "prefer": "safe",
+            "sec-ch-ua": "\"Not_A Brand\";v=\"8\", \"Chromium\";v=\"120\", \"Microsoft Edge\";v=\"120\"",
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": "\"Windows\"",
+            "sec-fetch-dest": "empty",
+            "sec-fetch-mode": "cors",
+            "sec-fetch-site": "same-origin",
+            "x-debug-options": "bugReporterEnabled",
+            "x-discord-locale": "fr",
+            "x-discord-timezone": "Indian/Reunion",
+            "x-super-properties": "eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiQ2hyb21lIiwiZGV2aWNlIjoiIiwic3lzdGVtX2xvY2FsZSI6ImZyIiwiYnJvd3Nlcl91c2VyX2FnZW50IjoiTW96aWxsYS81LjAgKFdpbmRvd3MgTlQgMTAuMDsgV2luNjQ7IHg2NCkgQXBwbGVXZWJLaXQvNTM3LjM2IChLSFRNTCwgbGlrZSBHZWNrbykgQ2hyb21lLzEyMC4wLjAuMCBTYWZhcmkvNTM3LjM2IEVkZy8xMjAuMC4wLjAiLCJicm93c2VyX3ZlcnNpb24iOiIxMjAuMC4wLjAiLCJvc192ZXJzaW9uIjoiMTAiLCJyZWZlcnJlciI6Imh0dHBzOi8vcGFsYWRpdW0tcHZwLmZyLyIsInJlZmVycmluZ19kb21haW4iOiJwYWxhZGl1bS1wdnAuZnIiLCJyZWZlcnJlcl9jdXJyZW50IjoiaHR0cHM6Ly9kaXNjb3JkLmNvbS9sb2dpbiIsInJlZmVycmluZ19kb21haW5fY3VycmVudCI6ImRpc2NvcmQuY29tIiwicmVsZWFzZV9jaGFubmVsIjoic3RhYmxlIiwiY2xpZW50X2J1aWxkX251bWJlciI6MjU5NTAxLCJjbGllbnRfZXZlbnRfc291cmNlIjpudWxsfQ=="
+        }
+
+        response = requests.get(url, headers=headers)
+
+        return response.json()
+    
+    def get_friends_names(self) -> list[str]:
+        friends = self.get_friends()
+        f = []
+
+        for friend in friends:
+            f.append(friend["user"]["global_name"])
+
+        return f
 
     def is_friend(self, user_id: str) -> bool:
         pass
