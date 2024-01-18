c29fde436779676093f41288a78f18048366275f
Filigram <130276494+Filigram@users.noreply.github.com>
2024-01-18 17:16:52 +0400

    Add get_own_infos func

--- discorduserapiVRequests.py
+++ discorduserapiVRequests.py
@@ -336,3 +336,31 @@ class DiscordUserAPI:
         if id in friends_ids:
             return True
         return False
+    
+    def get_own_infos(self) -> dict:
+        url = "https://discord.com/api/v9/users/@me"
+        headers = {
+            "accept": "*/*",
+            "accept-language": "fr,fr-FR;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
+            "authorization": "OTczNTQ1MTEwMjQ1NjI1ODY3.GOKbJ-.YZmX8sRn6jHk1NaTH8cvngYtqfko4mkeYYyR7A",
+            "cache-control": "no-cache",
+            "pragma": "no-cache",
+            "prefer": "safe",
+            "sec-ch-ua": "\"Not_A Brand\";v=\"8\", \"Chromium\";v=\"120\", \"Microsoft Edge\";v=\"120\"",
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": "\"Windows\"",
+            "sec-fetch-dest": "empty",
+            "sec-fetch-mode": "cors",
+            "sec-fetch-site": "same-origin",
+            "x-debug-options": "bugReporterEnabled",
+            "x-discord-locale": "fr",
+            "x-discord-timezone": "Indian/Reunion",
+            "x-super-properties": "eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiQ2hyb21lIiwiZGV2aWNlIjoiIiwic3lzdGVtX2xvY2FsZSI6ImZyIiwiYnJvd3Nlcl91c2VyX2FnZW50IjoiTW96aWxsYS81LjAgKFdpbmRvd3MgTlQgMTAuMDsgV2luNjQ7IHg2NCkgQXBwbGVXZWJLaXQvNTM3LjM2IChLSFRNTCwgbGlrZSBHZWNrbykgQ2hyb21lLzEyMC4wLjAuMCBTYWZhcmkvNTM3LjM2IEVkZy8xMjAuMC4wLjAiLCJicm93c2VyX3ZlcnNpb24iOiIxMjAuMC4wLjAiLCJvc192ZXJzaW9uIjoiMTAiLCJyZWZlcnJlciI6Imh0dHBzOi8vcGFsYWRpdW0tcHZwLmZyLyIsInJlZmVycmluZ19kb21haW4iOiJwYWxhZGl1bS1wdnAuZnIiLCJyZWZlcnJlcl9jdXJyZW50IjoiIiwicmVmZXJyaW5nX2RvbWFpbl9jdXJyZW50IjoiIiwicmVsZWFzZV9jaGFubmVsIjoic3RhYmxlIiwiY2xpZW50X2J1aWxkX251bWJlciI6MjU5NTAxLCJjbGllbnRfZXZlbnRfc291cmNlIjpudWxsfQ=="
+        }
+
+        response = requests.get(url, headers=headers)
+
+        return response.json()
+    
+    def set_status(self) -> int:
+        pass
