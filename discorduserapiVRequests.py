4bcbc9000682cac1e85c1de8aa3b72915cdc4b01
CodeurIII <113228775+codeuriii@users.noreply.github.com>
2024-01-19 11:29:59 +0400

    Add files via upload

--- discorduserapiVRequests.py
+++ discorduserapiVRequests.py
@@ -3,6 +3,7 @@ import requests
 import random
 import pygame
 from io import BytesIO
+import json
 
 
 class DiscordUserAPI:
@@ -362,5 +363,39 @@ class DiscordUserAPI:
 
         return response.json()
     
-    def set_status(self) -> int:
-        pass
+    def set_status(self, status: str) -> int:
+        """
+        The status can only be in this list: ["online", "offline", "inactive", "notdistrub"].
+        """
+        status_possibles = ["online", "offline", "inactive", "notdistrub"]
+        if not status in status_possibles:
+            raise ValueError(
+                'The status can only be in this list: ["online", "offline", "inactive", "notdistrub"].'
+            )
+        else:
+            with open("status.json", 'r') as f:
+                all_status = json.load(f)
+
+            url = "https://discord.com/api/v9/users/@me/settings-proto/1"
+            headers = {
+                "accept": "*/*",
+                "accept-language": "fr,fr-FR;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
+                "authorization": self.ouath,
+                "cache-control": "no-cache",
+                "content-type": "application/json",
+                "pragma": "no-cache",
+                "prefer": "safe",
+                "sec-ch-ua": "\"Not_A Brand\";v=\"8\", \"Chromium\";v=\"120\", \"Microsoft Edge\";v=\"120\"",
+                "sec-ch-ua-mobile": "?0",
+                "sec-ch-ua-platform": "\"Windows\"",
+                "sec-fetch-dest": "empty",
+                "sec-fetch-mode": "cors",
+                "sec-fetch-site": "same-origin",
+                "x-debug-options": "bugReporterEnabled",
+                "x-discord-locale": "fr",
+                "x-discord-timezone": "Indian/Reunion",
+                "x-super-properties": "eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiQ2hyb21lIiwiZGV2aWNlIjoiIiwic3lzdGVtX2xvY2FsZSI6ImZyIiwiYnJvd3Nlcl91c2VyX2FnZW50IjoiTW96aWxsYS81LjAgKFdpbmRvd3MgTlQgMTAuMDsgV2luNjQ7IHg2NCkgQXBwbGVXZWJLaXQvNTM3LjM2IChLSFRNTCwgbGlrZSBHZWNrbykgQ2hyb21lLzEyMC4wLjAuMCBTYWZhcmkvNTM3LjM2IEVkZy8xMjAuMC4wLjAiLCJicm93c2VyX3ZlcnNpb24iOiIxMjAuMC4wLjAiLCJvc192ZXJzaW9uIjoiMTAiLCJyZWZlcnJlciI6Imh0dHBzOi8vcGFsYWRpdW0tcHZwLmZyLyIsInJlZmVycmluZ19kb21haW4iOiJwYWxhZGl1bS1wdnAuZnIiLCJyZWZlcnJlcl9jdXJyZW50IjoiIiwicmVmZXJyaW5nX2RvbWFpbl9jdXJyZW50IjoiIiwicmVsZWFzZV9jaGFubmVsIjoic3RhYmxlIiwiY2xpZW50X2J1aWxkX251bWJlciI6MjU5NTAxLCJjbGllbnRfZXZlbnRfc291cmNlIjpudWxsfQ=="
+            }
+            payload = "{\"settings\":\"x280\"}".replace("x280", all_status[status])
+            response = requests.patch(url, headers=headers, data=payload)
+            return response.status_code
